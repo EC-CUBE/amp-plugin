4758de9ccf74d73cdf1a11b9cccb8e9e6e52de2a
kazumi_watanabe <kazumi_watanabe@lockon.co.jp>
2019-07-24 15:22:00 +0900

    URLの変更

--- PluginManager.php
+++ PluginManager.php
@@ -5,7 +5,7 @@
  *
  * copyright (c) EC-CUBE CO.,LTD. all rights reserved.
  *
- * http://www.lockon.co.jp/
+ * https://www.ec-cube.co.jp/
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
